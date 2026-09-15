98301156efd3f2afb158dfcaed4e5bdee43caf1e
Shyam1822 <125475536+Shyam1822@users.noreply.github.com>
2026-09-15 22:49:00 +0530

    Add: permutations - submission-1

---
+++ Data Structures & Algorithms/permutations/submission-1.py
@@ -0,0 +1,25 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        op = []
+        size = len(nums)
+        boolean_array = [False for _ in range(size)]
+        i = 0
+        def backtrack(boolean_array,path):
+            # base condition
+            # print(path)
+            if len(path)==len(nums):
+                op.append(path.copy())
+                return
+            for i in range(size):
+                # print(path,boolean_array)
+                if not boolean_array[i]:
+                    boolean_array[i]=True
+                    path.append(nums[i])
+                    backtrack(boolean_array,path)
+                    path.pop()
+                    boolean_array[i]=False
+        backtrack(boolean_array,[])
+        return op
+                
+            
+            
